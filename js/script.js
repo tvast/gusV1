9042d9117fde41de6ed23d5a40e106beec3b2cf2
Theophile <theophile@iMac-de-administrador-3.local>
2018-06-28 12:13:30 +0200

    v1 first commmit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+var originalString = document.getElementById("ascii1"); 
+
+function makeid() {
+  var text = "";
+  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  for (var i = 0; i < 10000; i++)
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+''
+  return originalString.innerHTML =(text);
+}
+
+console.log(makeid());
